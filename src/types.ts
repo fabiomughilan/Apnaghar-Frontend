755b54f489d798c8760f7e2bb01f396a23b61916
Fabio Mughilan <64077520+fabiomughilan@users.noreply.github.com>
2025-05-10 00:10:27 +0530

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type Property = {
+    id: number
+    title: string
+    location: string
+    rent: number
+    type: string
+    image: string
+    deposit: number
+    amenities: string[]
+  }
+  
