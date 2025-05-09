de1d32462bdce6f1b050f86fadb4c4a541bdc579
Fabio Mughilan <64077520+fabiomughilan@users.noreply.github.com>
2025-05-10 00:03:19 +0530

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer className="bg-white border-t mt-8">
+        <div className="container mx-auto px-4 py-6 text-center text-sm text-gray-500">
+          © 2025 ApnaGhar . All rights reserved. Developed by Fabio Mughilan
+        </div>
+      </footer>
+    )
+  }
+  
